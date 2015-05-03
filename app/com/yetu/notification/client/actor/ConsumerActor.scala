e83b4da534ecbc6a65f98d729e3000ea90202015
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-05-03 20:14:32 +0200

    Added consumer actor tests

--- app/com/yetu/notification/client/actor/ConsumerActor.scala
+++ app/com/yetu/notification/client/actor/ConsumerActor.scala
@@ -2,19 +2,23 @@ package com.yetu.notification.client.actor
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import com.github.sstone.amqp.Amqp.{Ack, Delivery}
-import com.yetu.notification.client.messages.MsgDeliver
+import com.yetu.notification.client.messages.{UnknownMsg, MsgDeliver}
+import play.api.Logger
 
 class ConsumerActor(messageListener: ActorRef) extends Actor with ActorLogging {
 
   override def receive: Receive = {
-    case Delivery(consumerTag, envelope, properties, body) =>
-      println("On on consume called")
+    case Delivery(consumerTag, envelope, properties, body) if body != null =>
+      Logger.debug(s"Delivery event happened with info envelope: $envelope with")
+      
       val msg: String = new Predef.String(body)
       messageListener ! MsgDeliver(msg)
 
       // for now Ack happens immediately, but MsgProcessed can be used
       sender ! Ack(envelope.getDeliveryTag)
-    case other => log.warning(s"Got other message $other")
+    case other =>
+      log.warning(s"Got other message $other")
+      sender ! UnknownMsg(other)
   }
 }
 
